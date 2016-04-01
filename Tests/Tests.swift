0db818f63d58323dfeba323eb7be7e01c105a2f3
Mathias Koehnke <mathias.koehnke@web.de>
2016-04-01 16:58:26 +0200

    Test Setup

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -22,29 +22,23 @@
 // THE SOFTWARE.
 
 import XCTest
+import WKZombie
 
 class Tests: XCTestCase {
     
+    var browser : WKZombie!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        browser = WKZombie(name: "WKZombie Tests")
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        browser = nil
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testClearCache() {
+        
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
