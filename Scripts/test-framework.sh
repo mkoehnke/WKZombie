17a208a9daf908747587e6dc041733a160ff1003
Mathias Köhnke <github@mathiaskoehnke.de>
2016-09-09 23:08:00 +0200

    TravisCI: iPhone7 Simulator not supported

--- Scripts/test-framework.sh
+++ Scripts/test-framework.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bash Scripts/setup-framework.sh
-xcodebuild -workspace WKZombie.xcworkspace -scheme WKZombie -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' build test
+xcodebuild -workspace WKZombie.xcworkspace -scheme WKZombie -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6s,OS=10.0' build test
