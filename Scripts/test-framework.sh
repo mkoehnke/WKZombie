28a2ddb49373edfdfd170c7e3cf13f8677a48511
Mathias Köhnke <github@mathiaskoehnke.de>
2016-09-09 22:26:52 +0200

    Fixed xcodebuild command

--- Scripts/test-framework.sh
+++ Scripts/test-framework.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 bash Scripts/setup-framework.sh
-xcodebuild -workspace WKZombie.xcworkspace -scheme WKZombie -sdk iphonesimulator build test
-
+xcodebuild -workspace WKZombie.xcworkspace -scheme WKZombie -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' build test
