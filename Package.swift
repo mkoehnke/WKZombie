4b016c48eeeddfa4e84996629397d4f1cef8bba6
Mathias Köhnke <github@mathiaskoehnke.de>
2016-09-19 23:07:03 +0200

    Initial commit for Command Line Example

--- Package.swift
+++ Package.swift
@@ -2,8 +2,11 @@ import PackageDescription
 
 let package = Package(
   name: "WKZombie",
-  targets: [],
+  targets: [
+      Target(name: "WKZombie"),
+      Target(name: "Example", dependencies:["WKZombie"])
+  ],
   dependencies: [
-	.Package(url: "https://github.com/mkoehnke/hpple.git", Version(0,2,2))
+	   .Package(url: "https://github.com/mkoehnke/hpple.git", Version(0,2,2))
   ]
 )
